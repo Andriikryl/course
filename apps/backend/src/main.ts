fcecfaa95ad01e852f659c5e839013c1bda1dd65
Andrii <andreyk557@gmail.com>
2026-05-15 20:22:45 +0300

    add swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+    const config = new DocumentBuilder()
+    .setTitle('Course API')
+    .setDescription('Fullstack course platform API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors();
   await app.listen(process.env.PORT ?? 3001);
--- apps/backend/src/modules/courses/dto/create-course.dto.ts
+++ apps/backend/src/modules/courses/dto/create-course.dto.ts
@@ -1,10 +1,19 @@
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateCourseDto {
+    @ApiProperty({
+    example: 'NestJS Fundamentals',
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+
+
+  @ApiPropertyOptional({
+    example: 'Backend course',
+  })
   @IsString()
   @IsOptional()
   description?: string;
