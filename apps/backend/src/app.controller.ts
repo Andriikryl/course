e8d6128dbb5faeb11c6a3701329f279ee68de00d
Andrii <andreyk557@gmail.com>
2026-06-06 18:23:40 +0300

    add swagger for controller

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateCourseDto } from './modules/courses/dto/create-course.dto';
 import { CoursesService } from './modules/courses/courses.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
-
+@ApiTags('Courses')
 @Controller('courses')
 export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
+  @ApiOperation({ summary: 'Get all courses' })
   @Get()
   getAll() {
     return this.coursesService.findAll();
@@ -17,6 +19,7 @@ export class CoursesController {
     return this.coursesService.findOne(Number(id));
   }
 
+  @ApiOperation({ summary: 'Create course' })
   @Post()
   create(@Body() dto: CreateCourseDto) {
     return this.coursesService.create(dto);
