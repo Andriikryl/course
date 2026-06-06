9298c42961db7434280f4bfc8711f32471db3306
Andrii <andreyk557@gmail.com>
2026-06-06 20:30:25 +0300

    add auth logic

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CoursesModule } from './modules/courses/courses.module';
 import { PrismaModule } from 'prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CoursesModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), CoursesModule, PrismaModule, AuthModule],
 })
 export class AppModule {}
---
+++ apps/backend/src/modules/auth/auth.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../../../prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  async register(email: string, password: string) {
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashed,
+      },
+    });
+
+    return this.signToken(user.id, user.email);
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) throw new UnauthorizedException('Invalid credentials');
+
+    return this.signToken(user.id, user.email);
+  }
+
+  signToken(userId: number, email: string) {
+    return {
+      access_token: this.jwt.sign({
+        sub: userId,
+        email,
+      }),
+    };
+  }
+}
